92487399738380871cb858f2edc301f85d5ab6d0
Chad Moyer <chad.moyer@trumbullcorp.com>
2017-03-01 13:10:14 -0500

    Project1 - Created an array that gathers our images based on a prefix.

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Chad Moyer on 3/1/17.
+//  Copyright © 2017 Chad Moyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
