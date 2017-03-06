1b4ed16f6507639c54fa3178d853414b086eedaf
Chad Moyer <chad.moyer@trumbullcorp.com>
2017-03-06 12:58:12 -0500

    Started Project2. Created a basic layout for our 3 buttons and put the ViewController inside a Navigation Controller.

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Chad Moyer on 3/6/17.
+//  Copyright © 2017 Chad Moyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
